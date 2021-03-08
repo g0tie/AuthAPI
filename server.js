38ddce8d17a1e88a6388455f3cbf47eac5d10b35
g0tie <gaultierbegue@outlook.com>
2021-03-08 12:05:22 +0400

    adding database connection

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
 require('dotenv').config()
 const app = require('express')();
+const connectToDb = require('./config/db');
+
+connectToDb();
 
 const listener = app.listen(process.env.PORT, () => console.log(`Server listening on port ${listener.address().port}`));
