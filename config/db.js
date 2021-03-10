386b415ed148566467d105a2224f9b730891e5aa
g0tie <gaultierbegue@outlook.com>
2021-03-10 10:57:33 +0400

    import .env vars for config

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDb = () => {
     mongoose.connect(
-        'mongodb://localhost:27017/AuthDB',
+        `mongodb://${process.env.DB_URL}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
         {
             useNewUrlParser: true,
             useUnifiedTypology: true
